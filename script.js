87869caa1da799b5a0a68271f97e848116226750
mdabidind <booksofkid@gmail.com>
2025-07-14 22:37:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Configure Tesseract.js to use local files
+Tesseract.workerOptions = {
+    workerPath: 'lib/tesseract-worker.min.js',
+    corePath: 'lib/tesseract-core.wasm.js',
+    langPath: 'tessdata/'
+};
 
+// Configure PDF.js to use local worker
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'lib/pdf.worker.min.js';
